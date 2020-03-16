5ed93cb8eeb53411e04af05ae5486eb48a0e75d8
Yejun Park <dogfeetguyz@gmail.com>
2020-03-16 22:57:51 +1100

    Bug fix when hiding keyboard

--- FLog/Common/KUIViewController.swift
+++ FLog/Common/KUIViewController.swift
@@ -44,6 +44,13 @@ class KUIViewController: UIViewController, UITextFieldDelegate {
             name: UIResponder.keyboardWillHideNotification,
             object: nil)
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
